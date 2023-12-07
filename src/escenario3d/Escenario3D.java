993da9cb00d174fb12701cf95c080a45dbb8f8ac
Javier05VR <mafervidalesr@gmail.com>
2023-12-07 16:36:38 -0600

    Escenario3D en Java

---
+++ src/escenario3d/Escenario3D.java
@@ -0,0 +1,14 @@
+
+package escenario3d;
+
+import java.awt.EventQueue;
+import views.Escenario;
+
+
+public class Escenario3D {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Escenario());
+	}
+}
+
---
+++ src/models/Point3D.java
@@ -0,0 +1,19 @@
+package models;
+
+
+public class Point3D extends Object3D {
+
+	public Point3D(String name, float x, float y, float z, float h) {
+		super(name, x, y, z, h);
+	}
+
+	public Point3D(String name, float x, float y, float z) {
+		super(name, x, y, z);
+	}
+
+	@Override
+	public String toString() {
+		return name + super.toString();
+	}
+
+}
